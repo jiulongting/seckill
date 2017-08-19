a4c99c6333f3a48a4ae0f9ddde9bd39a5569f90e
zhangjiulong <1@>
2017-08-19 14:24:41 +0800

    11

--- src/main/java/org/seckill/web/gitTest.java
+++ src/main/java/org/seckill/web/gitTest.java
@@ -1,13 +1,28 @@
 package org.seckill.web;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zhangjiulong on 2017/8/18.
  */
 public class gitTest {
-    public static void main(String[] args) {
+    private string s;
 
+    public string getS() {
+        return s;
+    }
+
+    public void setS(string s) {
+        this.s = s;
+    }
+
+    public static void main(String[] args) {
+        List t=new ArrayList();
         System.out.println("ddd");
 
     }
+
 }
 
